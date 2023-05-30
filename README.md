c71587f2bdd702dc77507e94cc3add0826aa45e3
ando <leo81@outlook.jp>
2023-05-30 09:49:42 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_use_dart3_demo
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/leoleo/template/flutter_use_dart3_demo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.leoleo.template.flutter_use_dart3_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
